5a98f19fe3dee250fea544cc69a04a712879c7fb
dhivya-kumaravel <jodhivne22@gmail.com>
2024-09-27 22:16:05 +0530

    changed

--- src/Component/Product.jsx
+++ src/Component/Product.jsx
@@ -4,16 +4,12 @@ const Product = ({product, cart, setCart}) => {
     const addCart = () => {
         setCart([...cart, product])}
 
-    const removeCart = () => {
-        setCart(cart.filter((c) => c.id !== product.id))
-    }
   return (
     <div className='cart border border-black w-40 items-center justify-center text-center p-2'>
         <div><img className='w-44 h-40' src={product.img} alt={product.name} /></div>
         <div><h2 className='font-bold'>{product.name}</h2>
         <p>Price Rs:{product.price}</p>
-        {cart.includes(product) ? <button className='border bg-red-600 p-1 rounded bg-slate-600 text-white hover:bg-green-500 hover:shadow-md hover:text-lg' onClick={removeCart}>Remove from Cart</button> : 
-        <button className='border border-4 border-yellow-600 p-1 rounded bg-slate-600 text-white hover:bg-green-500 hover:shadow-md hover:text-lg' onClick={addCart}>Add to Cart</button>}
+        <button className='border border-4 border-yellow-600 p-1 rounded bg-slate-600 text-white hover:bg-green-500 hover:shadow-md hover:text-lg' onClick={addCart}>Add to Cart</button>
         </div>
     </div>
   )
